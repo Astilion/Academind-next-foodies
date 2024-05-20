c0230b5b677cc355a45f93aa4cfc648970cc9b6e
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-05-20 17:48:03 +0200

    Changed to modules.css added active class using client

--- src/components/main-header/main-header.tsx
+++ src/components/main-header/main-header.tsx
@@ -1,18 +1,21 @@
+'use client'
 import Link from "next/link"
 import logoImg from '@/src/assets/logo.png'
 import Image from "next/image"
-import styles from './main-header.module.css'
+import classes from './main-header.module.css'
 import HeaderBackground from "./header-background"
+import { usePathname } from "next/navigation"
 export default function MainHeader() {
+    const path = usePathname()
     return (
         <>
             <HeaderBackground />
-            <header className="flex justify-between items-center py-8 px-4 md:px-[10%]"> <Link className="flex items-center justify-center gap-8 text-[#ddd6cb] font-bold font-mont tracking-widest text-2xl uppercase" href="/">
+            <header className={classes.header}> <Link className={classes.logo} href="/">
                 <Image className="w-20 h-20 object-contain drop-shadow-lg" src={logoImg} priority alt="A plate with food on it" /> NextLevel Food</Link>
 
-                <nav className={styles.nav}><ul className="m-0 p-0 flex gap-6 text-xl">
-                    <li><Link className="text-[#ddd6cb] font-bold py-2 px-4 rounded-lg" href="/meals">Browse Meals</Link></li>
-                    <li><Link className="text-[#ddd6cb] font-bold py-2 px-4 rounded-lg" href="/community">Foodies Community</Link></li>
+                <nav className={classes.nav}><ul className="m-0 p-0 flex gap-6 text-xl">
+                    <li><Link className={path.startsWith('/meals') ? classes.active : undefined} href="/meals">Browse Meals</Link></li>
+                    <li><Link className={path === '/community' ? classes.active : undefined} href="/community">Foodies Community</Link></li>
                 </ul></nav>
             </header></>)
 }
