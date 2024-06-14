9f7ab3c2e250715c6ec0e17e8e254e232ce6e250
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-06-14 11:01:26 +0200

    added not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFount() {
+    return <main className="not-found">
+        <h1>Not found</h1>
+        <p>Unfortunatelly, we could not find requested page or resource.</p>
+    </main>
+}
