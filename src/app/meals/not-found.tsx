a53040a225ecdd91037acb25e89449213fab2997
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-06-19 17:24:02 +0200

    Not found page for meals

---
+++ src/app/meals/not-found.tsx
@@ -0,0 +1,6 @@
+export default function NotFount() {
+    return <main className="not-found">
+        <h1>Meal not found</h1>
+        <p>Unfortunatelly, we could not find requested page or meal data.</p>
+    </main>
+}
