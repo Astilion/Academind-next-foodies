57ba4697bddd9929a169def3aae93799a5fb524e
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-06-19 17:38:33 +0200

    fixed title typo

--- src/app/meals/not-found.tsx
+++ src/app/meals/not-found.tsx
@@ -1,4 +1,4 @@
-export default function NotFount() {
+export default function NotFound() {
     return <main className="not-found">
         <h1>Meal not found</h1>
         <p>Unfortunatelly, we could not find requested page or meal data.</p>
