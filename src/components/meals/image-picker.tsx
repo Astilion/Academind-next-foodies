ad0b7ede1e4102b8a292321cdb044e44077716e5
Mateusz Kamiński <mkaminski.contact@gmail.com>
2024-06-26 11:07:55 +0200

    created client side image picker

---
+++ src/components/meals/image-picker.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { useRef, useState } from 'react'
+import classes from './image.picker.module.css'
+import Image from 'next/image'
+
+export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState()
+    const imageInputRef = useRef()
+    const handlePickClick = () => {
+        imageInputRef.current.click();
+    }
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+
+        if (!file) {
+            setPickedImage(null);
+            return;
+          }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => { 
+            setPickedImage(fileReader.result)
+        }
+        fileReader.readAsDataURL(file);
+    }
+    return (
+        <div className={classes.picker}>
+            <label htmlFor={name} >{label}</label>
+            <div className={classes.controls}>
+            <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage && <Image src={pickedImage} alt="The image selected by the user." fill/>}
+                </div>
+                <input
+                    className={classes.input}
+                    type="file"
+                    accept="image/pnt, image/jpeg"
+                    name={name}
+                    id={name}
+                    ref={imageInputRef}
+                    onChange={handleImageChange} 
+                    required/>
+                <button className={classes.button} type='button' onClick={handlePickClick}>Pick an Image</button>
+            </div>
+        </div>
+    )
+}
